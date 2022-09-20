51650d9dbf4b4e844893058aa8ba07ea5be43fe2
Kazeem Waris <Kazeemwaris05@gmail.com>
2022-09-20 07:34:22 -0700

    understanding % of negative in python

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+if number > 0:
+    lastNum = number % 10
+else:
+    lastNum = - ((- number) % 10)
+if (lastNum < 6) and (lastNum != 0):
+    print(f"Last digit of {number} is {lastNum} and is less than 6 and not 0") 
+elif lastNum > 5:
+    print(f"Last digit of {number} is {lastNum} and is greater than 5")
+else:
+    print(f"Last digit of {number} is {lastNum} and is 0")
