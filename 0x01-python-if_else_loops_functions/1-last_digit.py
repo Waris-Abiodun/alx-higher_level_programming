ede60c212495fdb5ba859a7792dd1ae98d44cdbd
Kazeem Waris <Kazeemwaris05@gmail.com>
2022-09-20 07:36:59 -0700

    understanding modulo of negative in python

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,7 +6,7 @@ if number > 0:
 else:
     lastNum = - ((- number) % 10)
 if (lastNum < 6) and (lastNum != 0):
-    print(f"Last digit of {number} is {lastNum} and is less than 6 and not 0") 
+    print(f"Last digit of {number} is {lastNum} and is less than 6 and not 0")
 elif lastNum > 5:
     print(f"Last digit of {number} is {lastNum} and is greater than 5")
 else:
