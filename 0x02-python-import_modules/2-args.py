28f7e20131e5110f449d90e6f014cf1ef267e3b2
Kazeem Waris <Kazeemwaris05@gmail.com>
2022-09-23 01:20:32 -0700

    playing with python argv[command line} , had enough fun with C argv,argc

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-import sys
-z = sys.argv
-i = 0
-while (i < len(z)):
-    if i == 0:
-        if len(z) == 1:
-            print("{} argument.".format(len(z) - 1))
-
-        elif len(z) == 2:
-            print("{} argument:".format(len(z) - 1))
+if __name__ == "__main__":
+    import sys
+    z = sys.argv
+    i = 0
+    while (i < len(z)):
+        if i == 0:
+            if len(z) == 1:
+                print("{} argument.".format(len(z) - 1))
+            elif len(z) == 2:
+                print("{} argument:".format(len(z) - 1))
+            else:
+                print("{} arguments:" .format(len(z) - 1))
         else:
-            print("{} arguments:" .format(len(z) - 1))
-    else:
-        print("{}: {}".format(i, z[i]))
-    i += 1
+            print("{}: {}".format(i, z[i]))
+        i += 1
