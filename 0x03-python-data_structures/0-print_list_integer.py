081a59eb6bb3c08dd7aa7c8c653002b12bbd513c
Kazeem Waris <Kazeemwaris05@gmail.com>
2022-09-25 06:11:36 -0700

    print_list_integer

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
     i = 0
-    b = my_list
-    while (i < len(b)):
-        print("{:d}".format(b[i]))
+    while (i < len(my_list)):
+        print("{:d}".format(my_list[i]))
         i += 1
