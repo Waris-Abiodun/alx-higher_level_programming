7353e702fae9bb88668e836e06aa6cc79222a940
Kazeem Waris <Kazeemwaris05@gmail.com>
2022-09-26 23:50:09 -0700

    maxNumber

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if my_list == None:
+    if my_list is None:
         return None
     i = 0
-    while (i < len(my_list) - 1):
+    while (i < len(my_list) - 2):
         if (my_list[i] > my_list[i + 1]):
             temp = my_list[i]
             my_list[i] = my_list[i + 1]
