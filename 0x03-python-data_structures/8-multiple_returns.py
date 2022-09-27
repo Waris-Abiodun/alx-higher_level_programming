3e7c221c69ccdefb1fd866196406708171ec5649
Kazeem Waris <Kazeemwaris05@gmail.com>
2022-09-26 23:01:16 -0700

    returning multiple  varaible

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,4 +1,12 @@
 def multiple_returns(sentence):
+    """
+    a function that return lenth of a sentence and its first character
+    Args:
+        sentence - the sentence to read
+    Return:
+        len(sentence) - lenght of a sentence
+        sentence[0] - the frist letter of the sentence
+    """
     if not sentence:
         return 0, None
     return len(sentence), sentence[0]
