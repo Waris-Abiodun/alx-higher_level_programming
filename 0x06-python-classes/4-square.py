2e87d0e105a416f65ca36c7deb88e9b6726068c4
Waris Abiodun <kazeemwaris05@gmail.com>
2022-10-13 16:00:11 -0700

    Getter and setter

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Square is an empty class with  square size"""
+
+
+class Square:
+    """Square is an empty class with  square size"""
+
+    def __init__(self, size=0):
+        """an empty constructor called size"""
+        if(type(size) is not int):
+            raise(TypeError("size must be an integer"))
+        elif(size < 0):
+            raise(ValueError("size must be >= 0"))
+        else:
+            self.__size = size
+
+    def area(self):
+        """Method to get the area of size raise to power 2"""
+        return(self.__size ** 2)
+    def size(self, value):
+        self.__size = value
+    def size(self):
+        return self.__size
