abe5a98d0ff387b64f3f570f71b1b4c000b02997
Waris Abiodun <kazeemwaris05@gmail.com>
2022-10-13 16:27:31 -0700

    printing a square using # and size

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""Square is an empty class with  square size"""
+
+
+class Square:
+    """Square is an empty class with  square size"""
+
+    def __init__(self, size=0):
+        """an empty constructor called size"""
+        if(type(size) is not int):
+            raise(TypeError("size must be an integer"))
+        elif(size < 0):
+            raise(ValueError("size must be >= 0"))
+        else:
+            self.__size = size
+
+    def area(self):
+        """Method to get the area of size raise to power 2"""
+        return(self.__size ** 2)
+
+    @property
+    def size(self):
+        """Getter for private size value"""
+        return(self.__size)
+
+    @size.setter
+    def size(self, value):
+        """setter for private size value"""
+        if(type(value) is not int):
+            raise(TypeError("size must be an integer"))
+        elif(value < 0):
+            raise(ValueError("size must be >= 0"))
+        else:
+            self.__size = value
+
+    def my_print(self):
+        """A function that print a square using # on the lengtj of size"""
+        for i range(self.size):
+            for j range(self.size):
+                print("#", end="")
+            print("#", end="")
+        print()
