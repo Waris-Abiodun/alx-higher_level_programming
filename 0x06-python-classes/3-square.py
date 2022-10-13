c4303352e3a95a3a35babb22515b962864d00ba6
Waris Abiodun <kazeemwaris05@gmail.com>
2022-10-13 15:36:40 -0700

    defining a class

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Square is an empty class with  square size"""
+
+
+class Square:
+    """Square is an empty class with  square size"""
+
+    def __init__(self, size=0):
+        """an empty constructor called size"""
+        if(type(size) is not int):
+            raise(TypeError("size must be an integer"))
+        elif(size < 0):
+            raise(ValueError("size must be >= 0"))
+        self.__size = size
+
+    def area(self):
+        return(self.size * self.size)
