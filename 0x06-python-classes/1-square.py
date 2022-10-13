952256e1b820271764fc015fb7e15ce2ac7e9782
Waris Abiodun <kazeemwaris05@gmail.com>
2022-10-13 09:06:30 -0700

    square

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -5,6 +5,6 @@
 class Square:
     """Square is an empty class with  square size"""
  
-    def __init__ size(self, size):
+    def __init__ size(self, square_size):
         """an empty constructor called size"""
-        self.__size = size
+        self.__size = square_size
