7d69f29cf3790e7382bef3b557df7a78f03826b5
Waris Abiodun <kazeemwaris05@gmail.com>
2022-10-13 15:39:47 -0700

    defining a class

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -11,7 +11,8 @@ class Square:
             raise(TypeError("size must be an integer"))
         elif(size < 0):
             raise(ValueError("size must be >= 0"))
-        self.__size = size
+        else:
+            self.__size = size
 
     def area(self):
         """Method to get the area of size raise to power 2"""
