adf028301fe9c21a2616f7f19f570e3bf8c9b002
Waris Abiodun <kazeemwaris05@gmail.com>
2022-10-13 08:42:02 -0700

    empty clas

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""Square is an empty class with  square size that is empty"""
-
 class Square:
-"""Square is an empty class with  square size that is empty"""
+
+    """Square is an empty class with  square size that is empty"""
     pass
