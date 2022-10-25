45417bcac9ff7c5127de7c76dedb85b1bf7aaa50
Waris Abiodun <kazeemwaris05@gmail.com>
2022-10-24 19:35:37 -0700

    checking for sub class and inherited

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -9,6 +9,6 @@ def inherits_from(obj, a_class):
     a function that return tru if they ae of the same class
     if it is inherit directly or indirectly
     """
-    if issubclass(obj, a_class) and type(obj) != a_class:
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
         return True
     return False
