e877f1195f878939795a4a4a7b332a1fe758386e
Waris Abiodun <kazeemwaris05@gmail.com>
2022-10-24 18:42:15 -0700

    getting the attribute of a class

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,3 +1,16 @@
 #!/usr/bin/python3
+
+"""
+a function that returns attribute of a class
+"""
+
+
 def lookup(obj):
+    """
+    a function that return the attribut of a class using dir
+    it return the list
+    Args:
+        obj(any) : object whose attribute will be returned
+    """
+
     return (dir(obj))
