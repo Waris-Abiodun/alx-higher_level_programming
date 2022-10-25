08b72e87c104c5e805e22ee316dc737ca5ca150d
Waris Abiodun <kazeemwaris05@gmail.com>
2022-10-24 19:26:51 -0700

    using isinstance

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+a function that return if obj and a are of the same class
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    a function that return tru if they ae of the same class
+    if it is an insance
+    """
+    return (isinstance(obj, a_class))
