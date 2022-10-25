7ff7c8fabea3329b8dcf07a07a22fe3bb7080c53
Waris Abiodun <kazeemwaris05@gmail.com>
2022-10-24 19:45:39 -0700

    method that raises an excepton that its not implemented

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+a class that work
+"""
+
+
+class BaseGeometry:
+    """
+    an classs that has many
+    """
+    def area(self):
+        """
+        a function that raises an error that its not implemented
+        """
+        raise("area() is not implemented")
