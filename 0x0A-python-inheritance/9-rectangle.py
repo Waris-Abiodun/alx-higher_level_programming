4a2bdd73d40b96d3db7dc99480dbc4f0a40c8f5d
Waris Abiodun <kazeemwaris05@gmail.com>
2022-10-24 20:34:49 -0700

    implemnt the area that was unimplemented earlier

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+a function that inherits geometry
+"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+    a function that use int validator in geometry to check for the
+    width and height of the rectangle
+    """
+
+    def __init__(self, width, height):
+        """
+        check for width and height and allocate value for them
+        """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+        self.__width = width
+        self.__height = height
+
+    def __str__(self):
+        return ("[{}] {}/{}".format(type(self).__name__,
+                                    self.__width, self.__height))
+
+    def area(self):
+        area = self.__width * self.__height
+        return (area)
