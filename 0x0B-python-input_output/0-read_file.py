d2ca194486fff80aa0e27c19bf1ca30a05a6c881
Waris Abiodun <kazeemwaris05@gmail.com>
2022-10-25 18:08:55 -0700

    reading a file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+a function that read text from a file
+"""
+
+
+def read_file(filename=""):
+    """
+    open a file and read it cntent
+    """
+    with open("my_file_0.txt", encoding="utf-8") as myfile:
+        print(myfile.read(), end="")
